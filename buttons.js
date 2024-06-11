111403e434591d95fca29335f8b21cb34c251d29
hassanalisheikh098 <143602821+hassanalisheikh098@users.noreply.github.com>
2024-06-11 14:52:06 +0500

    lets see 1

---
+++ buttons.js
@@ -0,0 +1,42 @@
+function redirectToPage() {
+    document.body.classList.add('fade-out');
+    setTimeout(function() {
+        window.location.href = '1.html';
+    }, 1000); // 1 second delay to match the fade-out duration
+}
+
+function redirectToPage2() {
+    document.body.classList.add('fade-out');
+    setTimeout(function() {
+        window.location.href = '2.html';
+    }, 1000); // 1 second delay to match the fade-out duration
+}
+
+function redirectToPage3() {
+    document.body.classList.add('fade-out');
+    setTimeout(function() {
+        window.location.href = '3.html';
+    }, 1000); // 1 second delay to match the fade-out duration
+}
+
+function redirectToPage4() {
+    document.body.classList.add('fade-out');
+    setTimeout(function() {
+        window.location.href = '4.html';
+    }, 1000); // 1 second delay to match the fade-out duration
+}
+
+function redirectToPage5() {
+    document.body.classList.add('fade-out');
+    setTimeout(function() {
+        window.location.href = '5.html';
+    }, 1000); // 1 second delay to match the fade-out duration
+}
+
+function redirectToPage6() {
+    document.body.classList.add('fade-out');
+    setTimeout(function() {
+        window.location.href = '6.html';
+    }, 1000); // 1 second delay to match the fade-out duration
+}
+
